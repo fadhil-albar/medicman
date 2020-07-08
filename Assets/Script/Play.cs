be3b59887cd2f6bd7bac01803f046825e74d01cb
fadhil-albar <fadhilalbar10@gmail.com>
2020-07-09 03:02:38 +0700

    up
    up

---
+++ Assets/Script/Play.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Play : MonoBehaviour
+{
+    public void next_intro()
+    {
+        SceneManager.LoadScene("intro1");
+    }
+
+    public void goto_avianflu()
+    {
+        SceneManager.LoadScene("intro_avianflu");
+    }
+
+    public void mulai_game1()
+    {
+        SceneManager.LoadScene("SampleScene");
+    }
+
+    public void mulai_game2()
+    {
+        SceneManager.LoadScene("level2");
+    }
+
+    public void quit_game()
+    {
+        Application.Quit();
+    }
+}
